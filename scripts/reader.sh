204b5f0973cbf989ab2d512dd2b9c57f89c7d77b
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2020-04-06 18:37:09 +0100

    [UPDATE] Source Code

--- scripts/reader.sh
+++ scripts/reader.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-# Purpose: Read Comma Separated CSV File
+# Purpose: Run Python
 # Author: Francisco Maria Calisto
 # ------------------------------------------
-cd dataset
-INPUT=overview.csv
-OLDIFS=$IFS
-IFS=','
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 100; }
-while read Age Contrast ContrastTag raw_input_path id tiff_name dicom_name
-do
-	echo "Age : $Age"
-	echo "Contrast : $Contrast"
-	echo "ContrastTag : $ContrastTag"
-	echo "raw_input_path : $raw_input_path"
-	echo "id : $id"
-	echo "tiff_name : $tiff_name"
-	echo "dicom_name : $dicom_name"
-done < $INPUT
-IFS=$OLDIFS
+
+python3 src/demo.py
--- src/demo.py
+++ src/demo.py
@@ -1,5 +1,27 @@
-import csv
-with open('dataset/overview.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""demo.py: a small and simple demo for this dataset."""
+
+__author__      = "Francisco Maria Calisto"
+__maintainer__  = "Francisco Maria Calisto"
+__email__       = "francisco.calisto@tecnico.ulisboa.pt"
+__license__     = "AGPLv3 & CC-BY-SA-4.0"
+__version__     = "0.1.0"
+__status__      = "Production"
+__copyright__   = "Copyright 2020, Instituto Superior Técnico (IST)"
+__credits__     = [
+	"Hugo Lencastre",
+	"Nádia Mourão",
+	"Bruno Dias",
+	"Luís Ribeiro Gomes",
+	"Bruno Oliveira",
+	"Carlos Santiago",
+	"Jacinto C. Nascimento",
+	"Pedro Miraldo",
+	"Nuno Nunes"
+]
+
+import pydicom
+
+# ==================== END File ==================== #
